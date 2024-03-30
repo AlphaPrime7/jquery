ed27ef5723a3ce5e4a396b99d17dd69d93135a16
alphaprime7 <awesome.tingwei@outlook.com>
2024-03-30 15:29:27 -0500

    added some java, had to know this cruel joke about java being different from JS

---
+++ hello.java
@@ -0,0 +1,10 @@
+//where java
+public class hello {
+        public static void main(String[] args) {
+          System.out.println("Hello World");
+        }
+}
+//being kneww deep into JS, simply meant i needed to know the difference between JS and Java
+//Now forest knight talks about this mean joke on the difference but it is just a historical relic
+//Yes it is a weird language but now I have written my first line in it
+//So far i have learnt if android == java else web = JS
