13cbecc3ccd280b4b542880f74377b2adf30fdbd
alphaprime7 <awesome.tingwei@outlook.com>
2024-03-30 23:07:10 -0500

    just some little additions to understand cursor behavior, next is seeing if i can add this to some functions i am using-issue is integrating jquery code into normal JS code; some nofun to tryout Monday-Happy weekend

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 //jquerry script syntax
 //Useful when trying to manipulate text areas.
-$(document).ready(function () {
+$(document).on('ready', function () {
     //your code here
   });
+
+$(function() {
+  $('#test').on('click', function(event){
+    cursor_position = event.pageY - $('#test').offset().top;
+    console.log(cursor_position);
+  if (cursor_position <= 15) {
+    document.getElementById('cl').innerHTML = 'Cursor on line 1';
+  } else {
+    document.getElementById('cl').innerHTML = 'Cursor NOT on line 1';
+  } 
+  });
+});
