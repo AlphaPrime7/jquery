b2b5f04f63d3945f2dc0e5ca242ebd2e739f62a7
alphaprime7 <awesome.tingwei@outlook.com>
2024-03-30 23:27:24 -0500

    Last setups for Monday

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ $(document).on('ready', function () {
     //your code here
   });
 
+//Works; integrate with normal JS code
 $(function() {
   $('#test').on('click', function(event){
     cursor_position = event.pageY - $('#test').offset().top;
@@ -15,3 +16,22 @@ $(function() {
   } 
   });
 });
+
+//To be tested
+$("textarea").live("keyup", function(e) {
+  if ($("textarea").attr("cols")) {
+      var cols = parseInt($("textarea").attr("cols")),
+          curPos = $('textarea').prop("selectionStart"),
+          result = Math.floor(curPos/cols);
+      var msg = (result < 1) ? "Cursor is on the First line!" : "Cursor is on the line #"+(result+1);
+      console.log($("p").text(msg).text());
+  };
+});​
+
+//Thinking of a way to count lines in a textarea
+var text="Hello world\n";
+if(text.indexOf("\n")==-1){
+  alert("No newline characters")
+}else{
+  alert("Contains newline characters")
+}
